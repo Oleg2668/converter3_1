011e3c0c9200cf16a49677a759fe575708a28792
Oleg <olegpanin1968@gmail.com>
2024-06-13 10:15:25 +0300

    Add fahrenheitto celsius converter

---
+++ src/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double c = 1;
+        double f = 100;
+
+        System.out.println("App for measures converting ");
+    }
+}
